2f7a4a3acdb46f84b9212e7395fbcc9eb87fa22f
Jiawen Chen <forward102910@gmail.com>
2021-01-08 00:52:58 +0000

    summary

--- summary.py
+++ summary.py
@@ -2,7 +2,7 @@
 # 2.flags: use timer defined in instance instead of flags to achieve animation.
 # 3.if two or more classes have similar parts, incorporate the similar part into the different one. 
 # Instead of creating different classes, pass in the different parameters into the function.
-# 4.settings is used to store the parameters that does not change throughtout the game.
+# 4.settings is used to store the parameters that does not change throughtout the game. In contrast, things in GameState are bond to change 
 # miscellaneous:
 # 5.state.active => is_running
 # 6.music
